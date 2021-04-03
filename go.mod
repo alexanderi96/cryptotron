07367ef54ed66fb0cff083ac409e64b0dab64e86
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2021-04-03 13:06:27 +0200

    Code cleanup.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alexanderi96/cryptotron
+
+go 1.16
+
+require github.com/NicoNex/echotron/v2 v2.2.4
--- main.go
+++ main.go
@@ -1,84 +1,46 @@
 package main
 
 import (
-	"github.com/NicoNex/echotron"
-	"github.com/alexanderi96/cryptotron/config"
-	"github.com/alexanderi96/cryptotron/api"
-	"fmt"
-	"strings"
-	"os"
-	"log"
-	"strconv"
 	"flag"
+	"log"
+	"github.com/NicoNex/echotron/v2"
 )
 
+type stateFn func(*echotron.Update) stateFn
+
 type bot struct {
-	chatId int64
-	echotron.Api
+	state stateFn
+	chatID int64
+	echotron.API
 }
 
-func newBot(api echotron.Api, chatId int64) echotron.Bot {
-    return &bot{
-            chatId,
-            api,
-        }
+var (
+	tgToken  string
+	cmcToken string
+)
+
+func newBot(chatID int64) echotron.Bot {
+	b := &bot{
+		chatID:       chatID,
+		API: echotron.NewAPI(tgToken),
+	}
+	b.state = b.handleMessage
+	return b
+}
+
+func (b *bot) handleMessage(update *echotron.Update) stateFn {
+	return nil
 }
 
 func (b *bot) Update(update *echotron.Update) {
-	path := "./archive/" + strconv.FormatInt(b.chatId, 10) + "/"
-	b.checkFolder(path)
-
-	log.Println("Message recieved from: " + strconv.FormatInt(b.chatId, 10))
-	if update.Message.Text == "/start" {
-	        b.SendMessage(fmt.Sprintf("*Welcome %s!*\nThis bot helps you monitoring cryptocurrencies\n\nTo use it type /data <currency code> <target currency code>\ne.g. /data BTC USD", update.Message.User.FirstName), b.chatId)
-
-	} else if update.Message.Text == "/data" {
-		textTmp := strings.Split(update.Message.Text, " ")
-
-		if (len(textTmp) > 2) {
-			b.SendMessage(api.CryptonatorGetData(textTmp[1], textTmp[2]), b.chatId)
-		} else {
-			b.SendMessage("Invalid argument: please specify the cryptocurrency and the target after the command", b.chatId)
-		}
-	} else if strings.Contains(update.Message.Text, "/market") {
-		textTmp := strings.Split(update.Message.Text, " ")
-
-		if (len(textTmp) > 2) {
-			b.SendMessage(api.CryptonatorGetMarket(textTmp[1], textTmp[2]), b.chatId)
-		} else {
-			b.SendMessage("Invalid argument: please specify the cryptocurrency and the target after the command", b.chatId)
-		}
-	
-	} else {
-		b.SendMessage("Invalid argument: please chose between the 2 functions /data or /market", b.chatId)
-	}
-}
-
-func (b *bot) checkFolder(path string) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		
-		os.Mkdir(path, 0755)
-		log.Println("Directory for user " + strconv.FormatInt(b.chatId, 10) + " created")
-	}
+	b.state = b.state(update)
 }
 
 func main() {
+	flag.StringVar(&tgToken, "tg", "", "The Telegram bot token")
+	flag.StringVar(&cmcToken, "cmc", "", "The CoinMarketCap api token")
+	flag.Parse()
 
-	values, err := config.ReadConfig("config.json")
-	var token string
-
-	if err != nil {
-		flag.StringVar(&token, "token", "", "The telegram bot token")
-		flag.Parse()
-		log.Println("Token is: " + token)
-		values.token = token
-	}
-
-	if _, err := os.Stat("archive"); os.IsNotExist(err) {
-		os.Mkdir("archive", 0755)
-	}
-
-	dsp := echotron.NewDispatcher(values.token, newBot)
-	log.Println("Running cryptotron")
-	dsp.Run()
+	dsp := echotron.NewDispatcher(tgToken, newBot)
+	log.Fatalln(dsp.Poll())
 }
